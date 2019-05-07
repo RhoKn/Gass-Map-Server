f011ee1cf8fdb773d9f763bff9390ce7403549cb
AMARO <andree0796@gmail.com>
2019-05-07 11:28:42 -0600

    Se ajustan regex y variables para separar mejor la informacion

--- controllers/gasStationsAuxController.js
+++ controllers/gasStationsAuxController.js
@@ -27,7 +27,7 @@ function cleanAddress(){
             "dieasel": ""
         },{
             "_id": "587fbd68edfe99480a072f77",
-            "calle": "Libramiento Tepic-Mazatlán No. 48  Col. Rodeo de la Punta  Tepic",
+            "calle": "Libramiento Nombre de Dios No. 48  Col. Rodeo de la Punta  Tepic",
             "rfc": "GOC9303301F4",
             "date_insert": "2017-01-18T19:09:26.860Z",
             "regular": "16.33",
@@ -43,7 +43,7 @@ function cleanAddress(){
             "dieasel": ""
         },{
             "_id": "587fbd68edfe99480a072f76",
-            "calle": "Av. Tercer Anillo Periférico No. 151  Colonia Primavera",
+            "calle": "Av. Colmena Nombre de dios No. 151  Colonia Primavera  Springfield",
             "rfc": "GAS060322BI6",
             "date_insert": "2017-01-18T19:09:26.860Z",
             "regular": "16.14",
@@ -67,27 +67,39 @@ function cleanAddress(){
     let colonia = "";
     let numero = "";
     let calle = "";
+    let ciudad = "";
 
     /*
         Regex
     */
-    let numeroRegex = /\No.+[0-9]/;
-    let coloniaRegex = /\Col.+/;
-    let calleRegex = /\No.+\s+\Col.+/;
-
+    let numeroRegex = /\No\.+\s+[0-9]+/;
+    let coloniaRegex = /\Col+[^a-np-z]+.+/;
+    let calleRegex = /\No\.+\s+[0-9]+\s+\Col+[^a-np-z]+.+/;
+    let ciudadRegex = /\Col.+\s\s/;
     /*
         Iteramos por cada elemento en el arreglo result
     */
     result.map(element => {
         // Se reemplaza la parte del string original que coincida con No.xxx Colxxx por nada para obtener solo la calle.
         calle = element.calle.replace(calleRegex, "");
-        console.log(calle);
 
         //Regex.exec retorna un arreglo con la parte del string que hizo match con la regex
         numero = numeroRegex.exec(element.calle)
-        console.log(numero[0])
         colonia = coloniaRegex.exec(element.calle)
-        console.log(colonia[0]);
+        ciudad = colonia.toString().replace(ciudadRegex, "");
+
+        ciudad = ciudad.toString();
+        colonia = colonia[0].toString().replace(ciudad, "");
+
+        calle = calle.toString();
+        numero = numero.toString();
+        colonia = colonia.toString();
+
+        console.log(calle);
+        console.log(numero);
+        console.log(colonia);
+        console.log(ciudad);
+        
     });
 }
 
