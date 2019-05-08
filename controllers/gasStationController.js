5f78597e608e9fae5c0cf6f2464c24049cf41011
Andree Amaro <andree0796@gmail.com>
2019-05-08 12:17:59 -0600

    Se agregan campos al modelo de estacion de gas para poder separar la direccion de una mejor manera

--- controllers/gasStationController.js
+++ controllers/gasStationController.js
@@ -15,24 +15,68 @@ function listAll(req, res){
       }
   };
 
+  let numeroRegex = /\No\.+.+[0-9]+/;
+  let coloniaRegex = /\Col+[^a-np-z]+.+/;
+  let calleRegex = /\No\.+.+[0-9]+.+/;
+  let ciudadRegex = /\Col.+\s\s/;
+
+  let colonia = "";
+  let numero = "";
+  let calle = "";
+  let ciudad = "";
+
   axios.get("https://api.datos.gob.mx/v2/precio.gasolina.publico")
   .then( response => {
     let json = response.data
 
     json.results.forEach( data => {
+
+      //console.log("data: " + data.calle);
+
+      calle = data.calle.replace(calleRegex, "");
+      calle = calle.toString();
+
+      numero = numeroRegex.exec(data.calle);
+      if(!numero){
+        numero = "S/N";
+      }
+      
+      numero = numero.toString();
+      
+      colonia = calleRegex.exec(data.calle);
+      if(!colonia){
+        colonia = "Not Available";
+      }else{
+        colonia = colonia.toString();
+        colonia = colonia.replace(numeroRegex, "");
+        colonia = colonia.replace("  ", "");
+      }
+
+      if(colonia == "Not Available"){
+        if(coloniaRegex.test(data.calle)){
+          colonia = coloniaRegex.exec(data.calle);
+          colonia = colonia.toString();
+
+          calle = calle.replace(colonia, "");
+        }
+      }
       let gas_station = new GasStation({
         id          :    data._id,
-        direction   :    data.calle,
+        street      :    calle,
+        number      :    numero,
+        colony      :    colonia,
         premium     :    data.premium,
         regular     :    data.regular,
         diesel      :    data.diesel
       })
 
+      console.log(gas_station);
+
       GasStation.find({ id: data._id }, (err, gs) => {
-        console.log(gs);
+        //console.log(gs);
         if (gs.length) {
           GasStation.remove({ id: data._id }).then(() => {
-            print('Se borro la gasolinera')
+            //print('Se borro la gasolinera')
           }).catch( (e) => {
             print(e)
           });
@@ -40,7 +84,7 @@ function listAll(req, res){
 
         gas_station.save()
         .then( () => {
-          print('Se guardo gasolinera')
+          //print('Se guardo gasolinera')
         }).catch((e) => {
           print(e)
         })
--- controllers/gasStationsAuxController.js
+++ controllers/gasStationsAuxController.js
@@ -4,62 +4,6 @@
 */
 
 function cleanAddress(){
-
-    /*
-        Sample
-    */
-    var result = [
-        {
-            "_id": "587fbd68edfe99480a072f14",
-            "calle": "Av. Adolfo López Mateos No. 1604  Col. Melchor Ocampo  Juárez",
-            "rfc": "DGA930823KD3",
-            "date_insert": "2017-01-18T19:09:26.784Z",
-            "regular": "15.71",
-            "colonia": "",
-            "numeropermiso": "PL/760/EXP/ES/2015",
-            "fechaaplicacion": "",
-            "permisoid": "2041",
-            "longitude": "-106.4514",
-            "latitude": "31.71947",
-            "premium": "17.93",
-            "razonsocial": "DÍAZ GAS  S.A. DE C.V.",
-            "codigopostal": "32380",
-            "dieasel": ""
-        },{
-            "_id": "587fbd68edfe99480a072f77",
-            "calle": "Libramiento Nombre de Dios No. 48  Col. Rodeo de la Punta  Tepic",
-            "rfc": "GOC9303301F4",
-            "date_insert": "2017-01-18T19:09:26.860Z",
-            "regular": "16.33",
-            "colonia": "",
-            "numeropermiso": "PL/420/EXP/ES/2015",
-            "fechaaplicacion": "07/01/2017 01:35:00 p. m.",
-            "permisoid": "2292",
-            "longitude": "-104.9266",
-            "latitude": "21.52014",
-            "premium": "18.1",
-            "razonsocial": "GRUPO OCTANO  S.A. DE C.V.",
-            "codigopostal": "63117",
-            "dieasel": ""
-        },{
-            "_id": "587fbd68edfe99480a072f76",
-            "calle": "Av. Colmena Nombre de dios No. 151  Colonia Primavera  Springfield",
-            "rfc": "GAS060322BI6",
-            "date_insert": "2017-01-18T19:09:26.860Z",
-            "regular": "16.14",
-            "colonia": "",
-            "numeropermiso": "PL/758/EXP/ES/2015",
-            "fechaaplicacion": "",
-            "permisoid": "2291",
-            "longitude": "-103.7026",
-            "latitude": "19.2684",
-            "premium": "17.9",
-            "razonsocial": "GASOPRIM S.A. DE C.V.",
-            "codigopostal": "28015",
-            "dieasel": ""
-        }
-    ]
-
     /*
         Variables donde se guardaran las partes de la direccion
         TODO: Cambiar esto para que se adecue con el modelo segun corresponda.
@@ -89,7 +33,7 @@ function cleanAddress(){
         ciudad = colonia.toString().replace(ciudadRegex, "");
 
         ciudad = ciudad.toString();
-        colonia = colonia[0].toString().replace(ciudad, "");
+        colonia = colonia[0].tocString().replace(ciudad, "");
 
         calle = calle.toString();
         numero = numero.toString();
